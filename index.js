9f4449c8b1900f63ea4b6fa54bc872ebac93524c
apufouzder <apufouzdar@gmail.com>
2021-09-14 09:50:20 -0400

    Node PATH, OS, FS, EVENT Module

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
-// Module System
-const people = require('./people');
-const _ = require('lodash');
+const path = require("path");
+const os = require("os");
+const fs = require("fs");
+// const EventEmitter = require("events");
 
-console.log(_.last(people));
+// require class from school.js file
+const School = require("./school");
 
+// Path
+const myPath = "D:/NodeJSPractice/people.js";
+console.log(path.basename(myPath));
+
+// Operating System
+console.log(os.networkInterfaces());
+
+// File System
+fs.writeFileSync("myFile.txt", "Hello Programmers!");
+fs.appendFileSync("myFile.txt", " How are you?");
+const data = fs.readFile("myFile.txt", (err, data) => {
+  console.log(data.toString());
+});
+console.log("object");
+
+const school = new School();
+// register a listener for bellRing event
+school.on("bellRing", ({ period, text }) => {
+  console.log(`We need to run ${period} ${text}!!`);
+});
+
+school.startPeriod();
